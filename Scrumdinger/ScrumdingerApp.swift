4eaa27c2ab4e105b91270c24b87a0c99e2b3490a
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-26 18:57:30 -0700

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Khanh Chung on 10/26/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
