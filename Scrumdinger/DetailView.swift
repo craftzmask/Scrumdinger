0cdfe0fa5002a0154fcdfe20b23f04197ca67004
Khanh Chung <khanh.d.chung@gmail.com>
2023-10-29 23:36:53 -0700

    implement navigation from DetailView to MeetingView.

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -12,9 +12,11 @@ struct DetailView: View {
     var body: some View {
         List {
             Section(header: Text("Meeting Info.")) {
-                Label("Start Meeting", systemImage: "timer")
-                    .foregroundColor(.accentColor)
-                    .font(.headline)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .foregroundColor(.accentColor)
+                        .font(.headline)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
@@ -38,6 +40,7 @@ struct DetailView: View {
                 }
             }
         }
+        .navigationTitle(scrum.title)
     }
 }
 
